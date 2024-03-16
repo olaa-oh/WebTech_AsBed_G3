26c6f518720897208cf892c52ddc23ce39bba008
AMMA ANNORBEA OFFEI - LARBI <amma.larbi@ashesi.edu.gh>
2024-03-16 00:57:18 +0000

    newly updated db

--- db/asbedDatabase.sql
+++ db/asbedDatabase.sql
@@ -1,69 +1,407 @@
-DROP DATABASE IF EXISTS asbedDatabase;
-CREATE DATABASE IF NOT EXISTS asbedDatabase;
-USE asbedDatabase;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 13, 2024 at 12:22 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- Rooms Table
-CREATE TABLE IF NOT EXISTS Rooms (
-    room_id INT AUTO_INCREMENT PRIMARY KEY,
-    room_name VARCHAR(100) NOT NULL,
-    capacity INT NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    is_available BOOLEAN DEFAULT TRUE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Roles Table
-CREATE TABLE IF NOT EXISTS Roles (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(100) NOT NULL
-);
+--
+-- Database: `asbedDatabase`
+--
 
+-- --------------------------------------------------------
 
--- Users Table
-CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-	email VARCHAR(255) NOT NULL unique,
-    role_id INT,
-    FOREIGN KEY (role_id) REFERENCES Roles(role_id)
-);
+--
+-- Table structure for table `Admin`
+--
 
--- Bookings Table
-CREATE TABLE IF NOT EXISTS Bookings (
-    booking_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    room_id INT,
-    start_time DATETIME,
-    end_time DATETIME,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (room_id) REFERENCES Rooms(room_id)
-);
+CREATE TABLE `Admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Schedule Table
-CREATE TABLE IF NOT EXISTS Schedule (
-    schedule_id INT AUTO_INCREMENT PRIMARY KEY,
-    room_id INT,
-    start_time DATETIME,
-    end_time DATETIME,
-    is_booked BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (room_id) REFERENCES Rooms(room_id)
-);
+--
+-- Dumping data for table `Admin`
+--
 
+INSERT INTO `Admin` (`admin_id`, `email`) VALUES
+(1, 'kwakukumi14@gmail.com');
 
--- Inserting sample roles
-INSERT INTO Roles (role_name) VALUES ('Admin'), ('Teacher'), ('Student');
+-- --------------------------------------------------------
 
--- Inserting sample rooms
-INSERT INTO Rooms (room_name, capacity, location, is_available) VALUES
-    ('Lion Hall', 50, 'Main Building', TRUE),
-    ('Elephant Hall', 40, 'East Wing', TRUE),
-    ('Zebra Hall', 30, 'West Wing', TRUE),
-    ('Giraffe Hall', 60, 'North Wing', TRUE),
-    ('Rhino Hall', 70, 'South Wing', TRUE);
-    
--- Inserting sample users
-INSERT INTO Users (username, password, email, role_id) VALUES
-    ('admin123', 'adminpassword', 'admin@example.com', 1),  -- Admin
-    ('teacher456', 'teacherpassword', 'teacher@example.com', 2),  -- Teacher
-    ('student789', 'studentpassword', 'student@example.com', 3);  -- Student
+--
+-- Table structure for table `Announcements`
+--
+
+CREATE TABLE `Announcements` (
+  `announcement_id` int(11) NOT NULL,
+  `sentTo` varchar(10) DEFAULT NULL,
+  `admin_id` int(11) DEFAULT NULL,
+  `announcement_text` text DEFAULT NULL,
+  `date_posted` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Halls`
+--
+
+CREATE TABLE `Halls` (
+  `hall_id` int(11) NOT NULL,
+  `hall_name` enum('Odeefoo Oteng Korankye Hall','Efua Sutherland','Ephraim Amu','Walter Sisulu','Hall 2C','Kofi Tawiah','Hall K') NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `location` enum('On campus','Off campus') NOT NULL,
+  `is_available` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Halls`
+--
+
+INSERT INTO `Halls` (`hall_id`, `hall_name`, `capacity`, `location`, `is_available`) VALUES
+(1, 'Odeefoo Oteng Korankye Hall', 10, 'On campus', 1),
+(2, 'Efua Sutherland', 10, 'On campus', 1),
+(3, 'Ephraim Amu', 10, 'On campus', 1),
+(4, 'Walter Sisulu', 10, 'On campus', 1),
+(6, 'Kofi Tawiah', 10, 'On campus', 1),
+(7, 'Hall K', 10, 'On campus', 1),
+(8, 'Hall 2C', 50, 'On campus', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Manager`
+--
+
+CREATE TABLE `Manager` (
+  `man_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Requests`
+--
+
+CREATE TABLE `Requests` (
+  `request_id` int(11) NOT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `subject` varchar(255) NOT NULL,
+  `request_text` text DEFAULT NULL,
+  `request_date` datetime DEFAULT current_timestamp(),
+  `is_resolved` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Roles`
+--
+
+CREATE TABLE `Roles` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Roles`
+--
+
+INSERT INTO `Roles` (`role_id`, `role_name`) VALUES
+(1, 'Admin'),
+(2, 'Manager'),
+(3, 'Student');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `RoomBookings`
+--
+
+CREATE TABLE `RoomBookings` (
+  `booking_id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rooms`
+--
+
+CREATE TABLE `Rooms` (
+  `room_id` int(11) NOT NULL,
+  `hall_id` int(11) DEFAULT NULL,
+  `room_name` varchar(100) NOT NULL,
+  `capacity` int(11) NOT NULL,
+  `is_available` tinyint(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Rooms`
+--
+
+INSERT INTO `Rooms` (`room_id`, `hall_id`, `room_name`, `capacity`, `is_available`) VALUES
+(1, 1, '608', 4, 1),
+(2, 7, 'K12', 2, 1),
+(5, 1, '601', 4, 1),
+(6, 6, 'D8', 2, 1),
+(7, 1, '601', 4, 1),
+(8, 6, 'D8', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Schedule`
+--
+
+CREATE TABLE `Schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `is_booked` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `has_room` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`user_id`, `username`, `password`, `email`, `role_id`, `has_room`) VALUES
+(1, 'Evans Kumi', '$2y$10$QrYHXXWIAhKGhC.0gr1tdeTKlt57tpkKVQ1G4j2rfzg/oJ7JbKiEC', 'kwakukumi14@gmail.com', 1, 0),
+(2, 'Naa Lamiorkor Boye', '$2y$10$eFdNHbsJ14C9Vt52H6/u7OkS/tfP.Bw/IqXDuwGnUZHtb8UEkKKFO', 'naalboye@gmail.com', 2, 0),
+(3, 'Amma Offei-Larbi', '$2y$10$/30b6Giu0X3d7TCvNl/aBe08ygiOKDJ/obCrsGxYUycnRpbzwlNCG', 'amma@gmail.com', 3, 0),
+(8, 'Lamie Boye', '$2y$10$.lbKjkw1jnpBMefazsnGhONFF1JFdHSCj0fOXeOUEC85cAX2uVwM2', 'lboye@gmail.com', 3, 0),
+(9, 'Abigail Nyarko', '$2y$10$UCGAWEI9Z30cJBRgPWgvI.gJhPA7WTSMy1awb3LwMuPeg2Lvx2Mzi', 'abigyalll@gmail.com', 3, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `VerifiablePins`
+--
+
+CREATE TABLE `VerifiablePins` (
+  `pin_id` int(11) NOT NULL,
+  `pin` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Admin`
+--
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `Announcements`
+--
+ALTER TABLE `Announcements`
+  ADD PRIMARY KEY (`announcement_id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `Halls`
+--
+ALTER TABLE `Halls`
+  ADD PRIMARY KEY (`hall_id`);
+
+--
+-- Indexes for table `Manager`
+--
+ALTER TABLE `Manager`
+  ADD PRIMARY KEY (`man_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `Requests`
+--
+ALTER TABLE `Requests`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `Roles`
+--
+ALTER TABLE `Roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `RoomBookings`
+--
+ALTER TABLE `RoomBookings`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `room_id` (`room_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `Rooms`
+--
+ALTER TABLE `Rooms`
+  ADD PRIMARY KEY (`room_id`),
+  ADD KEY `hall_id` (`hall_id`);
+
+--
+-- Indexes for table `Schedule`
+--
+ALTER TABLE `Schedule`
+  ADD PRIMARY KEY (`schedule_id`),
+  ADD KEY `room_id` (`room_id`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `VerifiablePins`
+--
+ALTER TABLE `VerifiablePins`
+  ADD PRIMARY KEY (`pin_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Admin`
+--
+ALTER TABLE `Admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Halls`
+--
+ALTER TABLE `Halls`
+  MODIFY `hall_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `Manager`
+--
+ALTER TABLE `Manager`
+  MODIFY `man_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Requests`
+--
+ALTER TABLE `Requests`
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Roles`
+--
+ALTER TABLE `Roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `RoomBookings`
+--
+ALTER TABLE `RoomBookings`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Rooms`
+--
+ALTER TABLE `Rooms`
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `Schedule`
+--
+ALTER TABLE `Schedule`
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `VerifiablePins`
+--
+ALTER TABLE `VerifiablePins`
+  MODIFY `pin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Announcements`
+--
+ALTER TABLE `Announcements`
+  ADD CONSTRAINT `announcements_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `Users` (`user_id`);
+
+--
+-- Constraints for table `Requests`
+--
+ALTER TABLE `Requests`
+  ADD CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `Users` (`user_id`);
+
+--
+-- Constraints for table `RoomBookings`
+--
+ALTER TABLE `RoomBookings`
+  ADD CONSTRAINT `roombookings_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `Rooms` (`room_id`),
+  ADD CONSTRAINT `roombookings_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`);
+
+--
+-- Constraints for table `Rooms`
+--
+ALTER TABLE `Rooms`
+  ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`hall_id`) REFERENCES `Halls` (`hall_id`);
+
+--
+-- Constraints for table `Schedule`
+--
+ALTER TABLE `Schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `Rooms` (`room_id`);
+
+--
+-- Constraints for table `Users`
+--
+ALTER TABLE `Users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `Roles` (`role_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
