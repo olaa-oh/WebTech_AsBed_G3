729d345da79be3b3b65e5ac8d76263590cb88d36
AMMA ANNORBEA OFFEI - LARBI <amma.larbi@ashesi.edu.gh>
2024-03-16 04:42:52 +0000

    delete request

---
+++ action/deleteRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once '../settings/connection.php';
+
+global $query;
+global $con;
+
+if(isset($_GET['id'])){
+    $r_id = $_GET['id'];
+    $query = "DELETE FROM requests WHERE request_id = $r_id ";
+    $choreSubject = "SELECT title FROM requests WHERE request_id = $r_id ";
+
+
+    //
+    if($con->query($choreSubject)){
+        $output = $con->query($choreSubject);
+
+        $row = $output->fetch_assoc();
+
+        $subReq = $row['title'];
+
+        $con->query($query);
+
+        echo '<script>alert("'.$subReq.' Deleted!");</script>';
+        echo '<script>window.location.href="../view/studentPortal.php";</script>';
+    }
+}
+else {
+    echo "end";
+    echo $query.$con->error;
+    exit();
+}
+
+
+?>
