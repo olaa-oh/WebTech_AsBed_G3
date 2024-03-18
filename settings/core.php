67d0a2635b7b6cc87e96b402b4ade13a6b1e17a8
Evans kumi <kwakukumi14@gmail.com>
2024-03-18 01:16:25 +0000

    resolved db names4

--- settings/core.php
+++ settings/core.php
@@ -9,7 +9,7 @@ function checkLogin() {
     // Check if user ID session exists
     if (!isset($_SESSION['user_id'])) {
         // If it doesn't exist, redirect to login_view page
-        header("Location: ../view/home.php");
+        header("Location: ../view/index.html");
         // Terminate script execution after redirection
         die();
     } else {
@@ -21,13 +21,13 @@ function checkLogin() {
         // If the session has been inactive for more than a minute (60 seconds), redirect to welcome page
         if ($elapsedTime >= 60) { // Change 60 to the desired number of seconds
             // Redirect to the login page  
-            header("Location: ../view/index.html");
+            header("Location: ../view/home.php");
             // Terminate script execution after redirection
             die();
         } else {
             // Update the last activity time to the current time
             $_SESSION['last_activity'] = $currentTime;
-            header("Location: ../view/home.php");
+            header("Location: ../view/index.html");
             die();
         }
     }
