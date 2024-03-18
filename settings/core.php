60286b1624f454924d57ce22398db782d90e1892
Evans kumi <kwakukumi14@gmail.com>
2024-03-18 01:11:08 +0000

    resolved db names2

--- settings/core.php
+++ settings/core.php
@@ -9,7 +9,7 @@ function checkLogin() {
     // Check if user ID session exists
     if (!isset($_SESSION['user_id'])) {
         // If it doesn't exist, redirect to login_view page
-        header("Location: ../view/landing.html");
+        header("Location: ../view/home.php");
         // Terminate script execution after redirection
         die();
     } else {
@@ -27,7 +27,7 @@ function checkLogin() {
         } else {
             // Update the last activity time to the current time
             $_SESSION['last_activity'] = $currentTime;
-            header("Location: ../view/student_portal.php");
+            header("Location: ../view/home.php");
             die();
         }
     }
